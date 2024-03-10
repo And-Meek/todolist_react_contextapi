2dbd390107364b770751e1d8a76e19864eaafbac
Andrew Meek <andrewmeek@MacBook-Air-AndMeek.local>
2024-03-11 01:10:26 +0300

    add loader

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 
 export const App = () => {
 	const [tasks, setTasks] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 	const [theme, setTheme] = useState('dark');
 	const changeTheme = () => {
 		if (theme === 'dark') {
@@ -14,21 +15,30 @@ export const App = () => {
 	};
 
 	useEffect(() => {
+		setIsLoading(true);
 		fetch('https://jsonplaceholder.typicode.com/todos')
 			.then((loadedData) => loadedData.json())
 			.then((loadedTasks) => {
 				setTasks(loadedTasks);
-			});
-	});
+			})
+			.catch((error) => {
+				console.log(error);
+			})
+			.finally(() => setIsLoading(false));
+	}, []);
 	return (
 		<div className={`${'App'} ${theme === 'dark' ? 'Dark' : 'Light'}`}>
 			<img src={themeIcon} className="img" alt="toggleTheme" onClick={changeTheme} />
 			<span>Todos list</span>
-			{tasks.map(({ userId, id, title, completed }) => (
-				<div className="List" key={id}>
-					{title}
-				</div>
-			))}
+			{isLoading ? (
+				<div className="loader"></div>
+			) : (
+				tasks.map(({ userId, id, title, completed }) => (
+					<div className="List" key={id}>
+						{title}
+					</div>
+				))
+			)}
 		</div>
 	);
 };
